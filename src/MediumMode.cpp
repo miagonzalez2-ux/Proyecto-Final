8713c8863553b578339db3ab294322bd79f5674e
miagonzalez2-ux <mia.gonzalez2@upr.edu>
2026-06-10 01:20:19 -0400

    Pushing cpp file for medium mode

---
+++ src/MediumMode.cpp
@@ -0,0 +1,14 @@
+#include "MediumMode.h"
+
+namespace medium {
+    Medium::Medium() : easy::Easy() { //defaulting the number of turns for medium game mode
+        maxTurn2 = maxTurn1 - 5;
+        if (maxTurn2 < 5)
+            maxTurn2 = 5;
+    }
+
+    int Medium::newMAX2(int maxTurn1, int& maxTurn2) { //setting the number of turns in medium game mode based off turns available in easy game mode
+        maxTurn2 = maxTurn1 - 5;
+        return maxTurn2;
+    }
+}
